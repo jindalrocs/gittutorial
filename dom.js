844b055e9771004f0b7ec9f0b9642e12674c35db
jindalrocs <jindalrocs@gmail.com>
2023-06-14 20:30:30 +0530

    dom file as youtuber

---
+++ dom.js
@@ -0,0 +1,31 @@
+//console.dir(document);
+//console.log(document.URL);
+//console.log(document.title);
+//console.log(document.all);
+//document.all[10].textContent = 'hello';
+//console.log(document.forms);
+
+//get element by ID
+//console.log(document.getElementById('header-title'));
+//var headerTitle = document.getElementById('header-title');
+//console.log(headerTitle);
+//headerTitle.textContent = 'hello';
+//headerTitle.innerText = 'good bye';
+//headerTitle.innerHTML ='<h3>hello</h3>';
+//headerTitle.style.borderBottom = 'solid 3px #000';
+var header =document.getElementById('main-header');
+header.style.borderBottom = 'solid 3px #000';
+
+//////GET ELEMENTS BY CLASS
+
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+items[1].textContent ='hello 2';
+//items[4]= 'Bold';
+var additem = document.getElementsByClassName('title');
+console.log(additem);
+additem[0].style.fontWeight = 'bold';
+additem[0].style.color = "green";
+
+
+console.log(items[1]);
